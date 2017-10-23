1ea76bd3df912a9aad1c857a4f76b1999869a344
ngu.nguyen@upskills.com <NGU.NGUYEN@upskills.com>
2017-10-23 10:58:50 +0700

    Optimize Exporting Process
    * Exporting raw data and Format cell are separated

--- src/upskills/fileexport/ExcelWriter.java
+++ src/upskills/fileexport/ExcelWriter.java
@@ -1,10 +1,10 @@
 package upskills.fileexport;
 
-import java.awt.Font;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.hssf.util.HSSFColor;
@@ -15,13 +15,11 @@ import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.ss.util.CellRangeUtil;
-import org.apache.poi.ss.util.CellUtil;
 import org.apache.poi.ss.util.RegionUtil;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import com.sun.prism.paint.Color;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTTable;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTTableStyleInfo;
 
 import jxl.Workbook;
 import jxl.write.Label;
@@ -167,7 +165,9 @@ public class ExcelWriter {
 		}
 	}
 
-	/** Format border and color background
+	/**
+	 * Format border and color background
+	 * 
 	 * @param wb
 	 * @param center
 	 * @param border
@@ -186,20 +186,19 @@ public class ExcelWriter {
 			cell_style.setBorderLeft(BorderStyle.MEDIUM);
 		}
 		if (color) {
-			
+
 			cell_style.setFillForegroundColor(color_val);
 			cell_style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-			
+
 		}
 	}
 
-	public static void formatFontCell(XSSFWorkbook wb,CellStyle cell_style)
-	{
+	public static void formatFontCell(XSSFWorkbook wb, CellStyle cell_style) {
 		org.apache.poi.ss.usermodel.Font font_style = wb.createFont();
 		font_style.setBold(true);
 		cell_style.setFont(font_style);
 	}
-	
+
 	public static void formatMergedCell(XSSFSheet sheet, Cell cell, CellRangeAddress merged_cell, boolean border,
 			boolean color, int color_val) {
 		if (border) {
@@ -215,21 +214,23 @@ public class ExcelWriter {
 		XSSFSheet sheet = workbook.createSheet(sheetname);
 		int rowNum = 0;
 		System.out.println("Creating excel ... XLsX");
-		// Create header 1
+		/*
+		 *  Create header 1
+		 */
 		CellRangeAddress cra1 = new CellRangeAddress(0, 0, 0, 6);
 		sheet.addMergedRegion(cra1);
-		
+
 		CellRangeAddress cra2 = new CellRangeAddress(0, 0, 7, 18);
 		sheet.addMergedRegion(cra2);
-		
+
 		CellRangeAddress cra3 = new CellRangeAddress(0, 0, 19, 20);
 		sheet.addMergedRegion(cra3);
 
 		Row headrow1 = sheet.createRow(rowNum++);
 		Cell cell1 = headrow1.createCell(0);
 		CellStyle cell_style1 = workbook.createCellStyle();
-		formatCell(workbook, cell_style1, true, true, true,  HSSFColor.ORANGE.index);
-		formatFontCell(workbook,cell_style1);
+		formatCell(workbook, cell_style1, true, true, true, HSSFColor.ORANGE.index);
+		formatFontCell(workbook, cell_style1);
 		cell1.setCellValue("MisMatch");
 		cell1.setCellStyle(cell_style1);
 		formatMergedCell(sheet, cell1, cra1, true, false, 0);
@@ -237,28 +238,28 @@ public class ExcelWriter {
 		Cell cell2 = headrow1.createCell(7);
 		cell2.setCellValue("Auto-Tagging");
 		CellStyle cell_style2 = workbook.createCellStyle();
-		formatCell(workbook, cell_style2, true, true, true,  HSSFColor.ROYAL_BLUE.index);
-		formatFontCell(workbook,cell_style2);
+		formatCell(workbook, cell_style2, true, true, true, HSSFColor.ROYAL_BLUE.index);
+		formatFontCell(workbook, cell_style2);
 		cell2.setCellStyle(cell_style2);
 		formatMergedCell(sheet, cell2, cra2, true, false, 0);
 
-		
 		Cell cell3 = headrow1.createCell(19);
 		cell3.setCellValue("User Input");
 		CellStyle cell_style3 = workbook.createCellStyle();
-		formatCell(workbook, cell_style3, true, true, true,  HSSFColor.SEA_GREEN.index);
-		formatFontCell(workbook,cell_style3);
+		formatCell(workbook, cell_style3, true, true, true, HSSFColor.SEA_GREEN.index);
+		formatFontCell(workbook, cell_style3);
 		cell3.setCellStyle(cell_style3);
 		formatMergedCell(sheet, cell3, cra3, true, false, 0);
-		
-		
+
 		int colNum = 0;
 
-		// Create header 2
+		/*
+		 *  Create header 2
+		 */
 		Row headrow = sheet.createRow(rowNum++);
 		CellStyle cell_head_style = workbook.createCellStyle();
-		formatFontCell(workbook,cell_head_style);
-		formatCell(workbook, cell_head_style, true, true, false,  (short) 0);
+		formatFontCell(workbook, cell_head_style);
+		formatCell(workbook, cell_head_style, true, true, false, (short) 0);
 		for (String header : IConstants.EXPORT_HEADER) {
 			Cell cell = headrow.createCell(colNum++);
 			cell.setCellValue(header);
@@ -267,19 +268,30 @@ public class ExcelWriter {
 		}
 
 		CellStyle cell_style = workbook.createCellStyle();
-		formatCell(workbook, cell_style, true, true, false,  (short) 0);
+		formatCell(workbook, cell_style, true, true, false, (short) 0);
+		List<String> conv_ob = new ArrayList<String>();
 		for (ResultObj obj : result) {
 			Row row = sheet.createRow(rowNum++);
 			colNum = 0;
-			List<String> conv_ob = obj.convertObj();
+			conv_ob.clear();
+			conv_ob = obj.convertObj();
 			for (String field : conv_ob) {
-				sheet.autoSizeColumn(colNum);
 				Cell cell = row.createCell(colNum++);
 				cell.setCellValue(field);
-				cell.setCellStyle(cell_style);
 
 			}
+//			System.out.println("Print row ... " + rowNum);
 		}
+
+		// Format exported table
+		for (int i = 2; i <= sheet.getLastRowNum(); i++) {
+			Row row = sheet.getRow(i);
+			for (Cell cell : row) {
+				cell.setCellStyle(cell_style);
+			}
+		}
+
+		// Export to file
 		try {
 			FileOutputStream outputStream = new FileOutputStream(filename);
 			workbook.write(outputStream);
